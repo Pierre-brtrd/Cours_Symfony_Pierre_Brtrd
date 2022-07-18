21886c87e18743b858a65c036e7ae33e46f4f04b
pierre.bertrand.professionnel <pierre.bertrand.professionnel@gmail.com>
2022-07-18 14:41:31 +0200

    feat: Add Fixtures bundle & add User

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AppFixtures extends Fixture
+{
+    private UserPasswordHasherInterface $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setEmail('pierre@test.com')
+            ->setPrenom('Pierre')
+            ->setNom('Bertrand')
+            ->setAddress('Nouvelle Maryland')
+            ->setZipCode(72350)
+            ->setVille('Chambéry')
+            ->setRoles(["ROLE_ADMIN"]);
+
+        $password = $this->hasher->hashPassword($user, 'test1234');
+        $user->setPassword($password);
+
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
