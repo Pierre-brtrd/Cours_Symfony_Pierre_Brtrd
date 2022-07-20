85761efd742a1682c0faea7e4351ee6aa57d67c9
pierre.bertrand.professionnel <pierre.bertrand.professionnel@gmail.com>
2022-07-20 14:50:48 +0200

    feat: Add CKEditor 4

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
 ];
--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -4,9 +4,9 @@ namespace App\Form;
 
 use App\Entity\Article;
 use App\Entity\Categorie;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -36,7 +36,7 @@ class ArticleType extends AbstractType
                 'download_uri' => false,
                 'image_uri' => true,
             ])
-            ->add('content', TextareaType::class, [
+            ->add('content', CKEditorType::class, [
                 'label' => 'Contenu:',
                 'required' => true,
             ]);
