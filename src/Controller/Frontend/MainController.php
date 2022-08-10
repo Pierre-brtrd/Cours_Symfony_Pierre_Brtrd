1fc05246d1efdfd1b100aab2ce56e01c2dc4336d
pierre.bertrand.professionnel <pierre.bertrand.professionnel@gmail.com>
2022-08-10 13:37:59 +0200

    fix(translation): update for global locale storage

--- src/Controller/Frontend/MainController.php
+++ src/Controller/Frontend/MainController.php
@@ -9,11 +9,6 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
-    #[Route(
-        '/{_locale<%app.supported_locales%>}',
-        defaults: ['_locale<%locale_en%>'],
-        name: 'home.switch.locale'
-    )]
     #[Route('/', name: 'home')]
     public function index(ArticleRepository $repository): Response
     {
---
+++ src/EventSubscriber/LocaleSubscriber.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class LocaleSubscriber implements EventSubscriberInterface
+{
+    public function __construct(
+        private string $defaultLocale = '<%locale%>'
+    ) {
+    }
+
+    public function onKernelRequest(RequestEvent $event)
+    {
+        $request = $event->getRequest();
+        if (!$request->hasPreviousSession()) {
+            return;
+        }
+
+        // try to see if the locale has been set as a _locale routing parameter
+        if ($locale = $request->attributes->get('_locale')) {
+            $request->getSession()->set('_locale', $locale);
+        } else {
+            // if no explicit locale has been set on this request, use one from the session
+            $request->setLocale($request->getSession()->get('_locale', $this->defaultLocale));
+        }
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            // must be registered before (i.e. with a higher priority than) the default Locale listener
+            KernelEvents::REQUEST => [['onKernelRequest', 20]],
+        ];
+    }
+}
