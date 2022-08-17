f8c89780715624ae5d94e097a11b26163996b883
pierre.bertrand.professionnel <pierre.bertrand.professionnel@gmail.com>
2022-08-17 13:33:21 +0200

    perf(cache): update cache for homepage

--- src/Controller/Frontend/MainController.php
+++ src/Controller/Frontend/MainController.php
@@ -6,17 +6,25 @@ use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 
 class MainController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(ArticleRepository $repository): Response
+    public function index(ArticleRepository $repository, CacheInterface $cache): Response
     {
-        $articles = $repository->findLatestArticleWithLimit(6);
+        $articles = $cache->get('home_articles_list', function (ItemInterface $item) use ($repository) {
+            $item->expiresAfter(1000);
 
-        return $this->render('Frontend/Home/index.html.twig', [
+            return $repository->findLatestArticleWithLimit(6);
+        });
+
+        $response = $this->render('Frontend/Home/index.html.twig', [
             'articles' => $articles,
             'curentPage' => 'home',
         ]);
+
+        return $response->setSharedMaxAge(1500);
     }
 }
